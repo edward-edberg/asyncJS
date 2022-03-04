0ba7ffdc23e74b481194031e9dac6f8fede67ad9
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-04 12:15:20 -0800

    first example of using API

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  // console.log(request.responseText);
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    console.log(data.flags);
+    console.log(data.flags['png']);
+    console.log(data.languages);
+    console.log(data.languages['ind']);
+    console.log(Object.keys(data.languages)[0]);
+    console.log(data.currencies[Object.keys(data.currencies)].name);
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags['svg']}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name['common']}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${
+          data.languages[Object.keys(data.languages)[0]]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          data.currencies[Object.keys(data.currencies)].name
+        }</p>
+
+      </div>
+    </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('india');
